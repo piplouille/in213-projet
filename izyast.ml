ab1231c2dbbb41166059ff709dffdaba9b7fcd38
Laetitia <debesse@ensta.fr>
2020-05-24 13:49:11 +0200

    on va y arriver

--- izyast.ml
+++ izyast.ml
@@ -19,10 +19,10 @@ type expr =
     | expr e -> print e
 ;;*)
 
-let rec print_matrice m = match m with
+(*let rec print_matrice m = match m with
     | [] -> ()
     | e::l -> print_int e ; print_string " " ; print_matrice l l
-;;
+;;*)
 
 let rec print oc = function
     | EString s -> Printf.fprintf oc "string \"%s\"" s
